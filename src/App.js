5eed5cea58249c7c611d4c0c9803db7f41b07fb4
ZAGHDOUDI2022 <zaghdoudimahdi@yahoo.com>
2022-11-22 02:26:50 +0100

    props_project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Profile from './profile/Profile';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
+  const profileElements =
+  {
+    fullName:"ZAGDOUDI MAHDI",
+    bio:"I falled in love with full-stuck js and I'm ready to tackle new challenges",
+    profession:"Full-stuck js",
+    imgSrc:"/315956907_641824051003270_6697787424361097939_n (1).jpg",
+    handleName:()=>{alert(`${profileElements.fullName}`)},
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Welcome</h1>
+      <Profile data={profileElements} handleName={profileElements.handleName}><img src={profileElements.imgSrc} alt="Profile pic"/></Profile>
+    </div >
   );
 }
 
